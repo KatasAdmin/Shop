8ff9f932b1606bae3ce7b82d3e537036d2cc111f
KatasAdmin <katasonoviktor@gmail.com>
2026-01-11 15:52:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from config import BOT_TOKEN
-from handlers import user_router, admin_router, manager_router
+from handlers import user_router, admin_router
+
 
 async def main():
     bot = Bot(token=BOT_TOKEN)
@@ -11,9 +12,9 @@ async def main():
 
     dp.include_router(user_router)
     dp.include_router(admin_router)
-    dp.include_router(manager_router)
 
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
