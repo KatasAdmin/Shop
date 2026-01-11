1140465509dad64264104883b8c52696914b8566
KatasAdmin <katasonoviktor@gmail.com>
2026-01-11 13:45:49 +0200

    Update states.py

--- states.py
+++ states.py
@@ -1,25 +1,18 @@
-# states.py
-
 from aiogram.fsm.state import StatesGroup, State
 
-
 class AdminFSM(StatesGroup):
-    add_category = State()
-
-    add_subcat_category = State()
-    add_subcat_name = State()
-
-    add_product_category = State()
-    add_product_subcategory = State()
-    add_product_name = State()
-    add_product_price = State()
-    add_product_description = State()
-    add_product_photos = State()
-
+    add_cat = State()
+    add_sub_cat = State()
+    add_sub_name = State()
+    prod_cat = State()
+    prod_sub = State()
+    prod_name = State()
+    prod_price = State()
+    prod_desc = State()
+    prod_photos = State()
     add_manager = State()
-
 
 class EditProductFSM(StatesGroup):
     name = State()
     price = State()
-    description = State()
+    desc = State()
