8299fa7244e795ed314db95608bf5fc2530c9989
KatasAdmin <katasonoviktor@gmail.com>
2026-01-11 13:44:50 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-# config.py
 import os
 from dotenv import load_dotenv
 
@@ -6,7 +5,8 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
-PAYMENT_SIMULATION = os.getenv("PAYMENT_SIMULATION", "true").lower() == "true"
 
 DATA_FILE = "data.json"
 LOCK_FILE = "/tmp/bot.lock"
+
+PAYMENT_SIMULATION = os.getenv("PAYMENT_SIMULATION", "1") == "1"
