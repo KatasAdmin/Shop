7306d7cdf8f7af2dcc7407f821f68f73cd6d1811
KatasAdmin <katasonoviktor@gmail.com>
2026-01-11 13:48:37 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,5 @@
-# TODO
+from .user import router as user_router
+from .admin import router as admin_router
+from .manager import router as manager_router
+
+__all__ = ["user_router", "admin_router", "manager_router"]
