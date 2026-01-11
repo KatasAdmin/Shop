a8f93ea372a2d6541fff2d4deae611ba6118d9c4
KatasAdmin <katasonoviktor@gmail.com>
2026-01-11 15:47:45 +0200

    Update states.py

--- states.py
+++ states.py
@@ -1,15 +1,11 @@
-# states.py
-
 from aiogram.fsm.state import StatesGroup, State
 
 
 class AdminFSM(StatesGroup):
-    # Категории / подкатегории
     add_cat = State()
     add_sub_cat = State()
     add_sub_name = State()
 
-    # Товары (добавление)
     prod_cat = State()
     prod_sub = State()
     prod_name = State()
@@ -17,22 +13,19 @@ class AdminFSM(StatesGroup):
     prod_desc = State()
     prod_photos = State()
 
-    # Менеджеры
     add_manager = State()
 
 
 class EditProductFSM(StatesGroup):
-    # Редактирование товара
     name = State()
     price = State()
     desc = State()
 
 
 class OrderFSM(StatesGroup):
-    # Оформление заказа (после checkout)
-    name = State()              # ФИО/имя
-    phone = State()             # телефон (контакт или вручную)
-    city = State()              # город
-    delivery_method = State()   # способ доставки (НП/курьер)
-    delivery_point = State()    # отделение/почтомат или адрес
-    comment = State()           # комментарий/примечание
+    name = State()
+    phone = State()
+    city = State()
+    delivery_method = State()
+    delivery_point = State()
+    comment = State()
