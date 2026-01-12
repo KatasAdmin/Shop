6e355a04f263ed420e5c26881250ef6f27c71a99
KatasAdmin <katasonoviktor@gmail.com>
2026-01-12 20:02:18 +0200

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,5 +1,6 @@
 from db import engine, Base
-import models  # важливо: щоб моделі зареєструвались у Base.metadata
+import models  # важливо: щоб моделі підвантажились
+
 
 async def init_db():
     async with engine.begin() as conn:
