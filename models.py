d2b233f62c025596e7e6262b0a1d566f8d95cc61
KatasAdmin <katasonoviktor@gmail.com>
2026-01-14 23:50:27 +0200

    Update models.py

--- models.py
+++ models.py
@@ -14,7 +14,7 @@ class KVStore(Base):
     key: Mapped[str] = mapped_column(String(64), primary_key=True)
     value: Mapped[dict] = mapped_column(JSONB, nullable=False)
 
-    updated_at: Mapped[str] = mapped_column(
+    updated_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         server_default=func.now(),
         onupdate=func.now()
