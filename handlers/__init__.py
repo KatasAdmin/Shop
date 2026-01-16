bb259f5b788c983879b06559a676bb4188f2a72d
KatasAdmin <katasonoviktor@gmail.com>
2026-01-16 08:01:40 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,13 @@
 # handlers/__init__.py
 from .user import router as user_router
 
-from .admin_orders import router as admin_orders_router
-# (пізніше додамо admin_catalog_router, admin_users_router)
+# старий адмін (якщо він у тебе був)
+from .admin import router as admin_router
 
-__all__ = ["user_router", "admin_orders_router"]
+# новий модуль замовлень (додасться коли файл точно буде на місці)
+try:
+    from .admin_orders import router as admin_orders_router
+except Exception:
+    admin_orders_router = None
+
+__all__ = ["user_router", "admin_router", "admin_orders_router"]
